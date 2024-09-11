fb3a8baf4863aa7ceadd3486ff5a54f53de4521f
omeraltan <omeraltan66@gmail.com>
2024-09-11 11:10:34 +0300

    Commit All

---
+++ ioc-and-di-in-spring/src/main/java/com/omer/iocanddi/chapter3/beannaming/BeanNamingCfg.java
@@ -0,0 +1,31 @@
+package com.omer.iocanddi.chapter3.beannaming;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(nameGenerator = SimpleBeanNameGenerator.class)
+public class BeanNamingCfg {
+
+
+    @Bean
+    public SimpleBean anotherSimpleBean(){
+        return new SimpleBean();
+    }
+
+    // @Bean(name= "simpleBeanTwo")
+    // @Bean(value= "simpleBeanTwo")
+    @Bean("simpleBeanTwo")
+    public SimpleBean simpleBean2(){
+        return new SimpleBean();
+    }
+
+    // @Bean(name= "simpleBean_tres")
+    // @Bean(value= "simpleBean_tres")
+    @Bean({"simpleBeanThree", "three", "numero_tres"})
+    public SimpleBean simpleBean3(){
+        return new SimpleBean();
+    }
+
+}
---
+++ ioc-and-di-in-spring/src/main/java/com/omer/iocanddi/chapter3/beannaming/SimpleBean.java
@@ -0,0 +1,7 @@
+package com.omer.iocanddi.chapter3.beannaming;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "simpleBeanOne")
+public class SimpleBean {
+}
